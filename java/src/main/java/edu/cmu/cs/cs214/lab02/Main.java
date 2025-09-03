8cc30dd2fb708c45553f62b7278abb4e1f41764b
yaqing-zhou <yaqingz@andrew.cmu.edu>
2025-09-03 09:50:00 -0400

    refactor(java/): rewrite the java implementation against the shape interface to improve encapsulation

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,22 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape shape1 = new Rectangle(2, 3);
+        Shape shape2 = new Circle(2);
+        Shape shape3 = new Square(2);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer renderer1 = new Renderer(shape1);
+        Renderer renderer2 = new Renderer(shape2);
+        Renderer renderer3 = new Renderer(shape3);
 
-        renderer.draw();
+        renderer1.draw();
+        renderer2.draw();
+        renderer3.draw();
     }
 }
